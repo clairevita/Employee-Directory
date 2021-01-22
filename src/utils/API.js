f036bbb5f8ae05a2df4144a0d544e4ef78372768
Claire Vita <clairej.vita@gmail.com>
2021-01-21 23:19:01 -0500

    increased api dataset for diversity

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   getRandomUser: function() {
-    return axios.get("https://randomuser.me/api/?results=20&nat=us");
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
