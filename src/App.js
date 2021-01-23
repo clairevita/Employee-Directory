0619de38536fe6916efc60241d235034dbc61fb9
Claire Vita <clairej.vita@gmail.com>
2021-01-23 01:34:14 -0500

    prettied

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ import Wrapper from './components/Wrapper';
 
 function App() {
   return (
-    <div className="App">
-      
    <Wrapper>
-   
      <Main />
    </Wrapper>
-    </div>
   );
 }
 
